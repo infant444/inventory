6983b5e3a284f73ec23ede8151523364923e251b
infant04 <infant0467@gmail.com>
2026-01-23 20:18:26 +0530

    add

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://inventory-z6w5.onrender.com';
+const API_BASE_URL = 'https://inventory-z6w5.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
