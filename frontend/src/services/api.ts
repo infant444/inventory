a4cce445a25d0816133a4012bb0eaf134e2e4759
infant04 <infant0467@gmail.com>
2026-02-28 21:28:11 +0530

    feat: Switch API base URL to local development environment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://inventory-z6w5.onrender.com/api';
+const API_BASE_URL = 'https://inventory-qj08.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
