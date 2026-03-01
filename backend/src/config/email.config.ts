cd85ac3d263f36976e6ea825f0edc2c903be1e24
infant04 <infant0467@gmail.com>
2026-03-01 19:13:23 +0530

    abc

--- backend/src/config/email.config.ts
+++ backend/src/config/email.config.ts
@@ -6,12 +6,16 @@ import Mailgen from "mailgen";
 // ======================
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for 587
   auth: {
-    user: process.env.EMAIL_USER!,
-    pass: process.env.EMAIL_PASSWORD!,
+    user: process.env.MAIL,
+    pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false
   }
-
 });
 
 // Verify connection (optional but helpful)
