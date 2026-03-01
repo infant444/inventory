d4a1e2618bdcc9c9757d15d3c551e6070e5dc134
puviyarasans <puviyarasansasi123@gmail.com>
2026-03-01 21:00:56 +0530

    abc

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://inventory-qj08.onrender.com/api';
+const API_BASE_URL = 'https://inventory-fwcz.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
